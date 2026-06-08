1d12355629fd4e7e4f7f59c347ffb2164291c7e9
Chris Harrington <chrisharrington99@gmail.com>
2026-06-07 20:17:55 -0600

    [BTAPI-11] Convert class-based models to types/interfaces. (#10)

--- server/lib/models.ts
+++ server/lib/models.ts
@@ -1,112 +1,44 @@
-import { ExpoPushErrorTicket, ExpoPushSuccessTicket } from 'expo-server-sdk';
-
-export class Id {
-    _id!: string;
+export interface Id {
+    _id: string;
 }
 
-export class Device extends Id {
-    token!: string;
-
-    static fromRaw(raw: any) : Device {
-        const device = new Device();
-        device._id = raw._id;
-        device.token = raw.token;
-        return device;
-    }
+export interface Device extends Id {
+    token: string;
 }
 
-export class Budget {
-    date!: Date;
-    weeklyAmount!: number;
-    balance!: number;
-    transactions!: Transaction[];
-    
-    constructor(initializer: Partial<Budget>) {
-        Object.assign(this, initializer);
-    }
+export interface Budget {
+    date: Date;
+    weeklyAmount: number;
+    balance?: number;
+    transactions: Transaction[];
 }
 
-export class Transaction extends Id {
-    amount!: number;
-    date!: Date;
-    description!: string;
-    owner!: string;
-    ignored!: boolean;
+export interface Transaction extends Id {
+    amount: number;
+    date: Date;
+    description: string;
+    owner: string;
+    ignored: boolean;
     tags: Tag[];
     isAllowancePayment?: boolean;
-
-    constructor() {
-        super();
-        this.tags = [];
-    }
-
-    static fromRaw(raw: any) : Transaction {
-        const transaction = new Transaction();
-        transaction._id = raw._id;
-        transaction.amount = raw.amount;
-        transaction.date = new Date(raw.date);
-        transaction.description = raw.description;
-        transaction.owner = raw.owner;
-        transaction.ignored = raw.ignored;
-        transaction.tags = raw.tags || [];
-        return transaction;
-    }
-
-    static fromMessage(raw: string, date: Date) : Transaction {
-        let parsed = raw.substring(raw.indexOf('$'));
-        const words = parsed
-            .substring(0, parsed.indexOf('<br'))
-            .replace('<sup>', '')
-            .replace('</sup>', '')
-            .replace('&#174;', '')
-            .split(' ')
-            .filter(w => w.trim().length > 0);
-
-        const transaction = new Transaction();
-        transaction.amount = parseFloat(words[0].replace('$', '').replace(',', ''));
-        transaction.date = date;
-        transaction.description = words.slice(words.indexOf('at')+1, words.lastIndexOf('on')).join(' ');
-        transaction.owner = parsed.indexOf('1379') > -1 ? 'Chris' : 'Sarah';
-        transaction.ignored = false;
-        return transaction;
-    }
-    
-    static copy(t: Transaction) : Transaction {
-        const copy = new Transaction();
-        copy.amount = t.amount;
-        copy.date = t.date;
-        copy.description = t.description;
-        copy.owner = t.owner;
-        copy.ignored = t.ignored;
-        copy.tags = [...t.tags];
-        return copy;
-    }
 }
 
-export class Tag extends Id {
-    name!: string;
-    ignore!: boolean;
+export interface Tag extends Id {
+    name: string;
+    ignore: boolean;
 }
 
-export class NotificationTicket extends Id {
-    status!: string;
-    notificationId!: string;
-    receiptAcquired!: boolean;
-
-    static fromRaw(raw: any) : NotificationTicket {
-        const ticket = new NotificationTicket();
-        ticket.status = raw.status;
-        ticket.notificationId = raw.id;
-        ticket.receiptAcquired = false;
-        return ticket;
-    }
+export interface NotificationTicket extends Id {
+    status: string;
+    notificationId: string;
+    receiptAcquired: boolean;
 }
 
-export class Balance extends Id {
-    weekOf!: Date;
-    amount!: number;
+export interface Balance extends Id {
+    weekOf: Date;
+    amount: number;
 }
 
-export class OneTime extends Id {
-    balance!: number;
+export interface OneTime extends Id {
+    balance: number;
 }
